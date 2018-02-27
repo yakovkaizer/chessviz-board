abecc220eba024602c3bfe0e372e0a460398278e
Кайзер Яков Иванович <ip711s10@mwp11.csc.local>
2018-02-27 12:59:19 +0700

    created a chess board

---
+++ chessviz.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	printf("┏━━━━━━━━┓\n");
+	printf("|rnbqkbnr|\n");
+	printf("|pppppppp|\n");
+	printf("| █ █ █ █|\n");
+	printf("|█ █ █ █ |\n");
+	printf("| █ █ █ █|\n");
+	printf("|PPPPPPPP|\n");
+	printf("|RNBQKBNR|\n");
+	printf("┗━━━━━━━━┛\n");
+return 0;
+}
